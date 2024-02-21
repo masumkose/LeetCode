0cfb021f1ac7701ea8525f2708bf8ee2394f2faf
Muhammet Kose <muhakose@1-C-18.42heilbronn.de>
2024-02-21 09:01:11 +0100

    isMatch solution

--- isMatch/isMatch.c
+++ isMatch/isMatch.c
@@ -2,52 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
 bool isMatch(char* s, char* p)
 {
-    if (*p == '\0' && *s == '\0')
-        return true;
+	if (*p == '\0' && *s == '\0')
+		return true;
 
-    
-    if ((*s == *p || *p == '.') && *s && *p)
-    {
-        if (isMatch(s + 1, p + 1))//this is the buffer error
-            return (true);
-    }
+	if (*p == '\0')
+		return false;
 
-    if (*(p + 1))
-    {
-        if (*(p + 1) == '*')
-        {
-            while ((*s == *p || *p == '.') && *s && *p)
-            {
-                if (*s && *(p + 2))
-                {
-                    if (isMatch(s, p + 2))
-                        return true;
-                }
-                s++;
-            }
-            if (*(p + 2) != '\0' && *s != '\0')
-            {
-                if (isMatch(s , p + 2))
-                    return (true);
+	if (*(p + 1) == '*' && (*s != *p || *p != '.'))
+	{
+		if (isMatch(s, p + 2))
+			return true;
+	}
+	if ((*s == *p || *p == '.') && *s)
+	{	
+		if (*(p + 1) == '*')
+		{
+			if (isMatch(s + 1, p))
+				return true;
+		}
+		if (isMatch(s + 1, p + 1))
+			return (true);
+	}
 
-            }
-            else if (*(p + 2) == '\0' && *s == '\0')
-                return true;
-        }
-    }
-    return (false);
+	return (false);
 }
 
 int main()
 {
 
-    if (isMatch("a","ab*"))
-        printf("TRUE");
-    else
-        printf("FALSE");
+	if (isMatch("a","ab*"))
+		printf("TRUE");
+	else
+		printf("FALSE");
 
 
-    return 0;
+	return (0);
 }
