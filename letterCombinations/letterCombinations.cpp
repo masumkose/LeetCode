b5321afe237325b40aa5d8be09d085819cc5da1c
Muhammet Kose <muhakose@1-C-1.42heilbronn.de>
2024-06-04 11:37:29 +0200

    letter

--- letterCombinations/letterCombinations.cpp
+++ letterCombinations/letterCombinations.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -9,25 +11,39 @@ class Solution
 public:
 	vector<string> letterCombinations(string digits)
 	{
-		vector<string> result;
-
-
-
+		if (digits.empty()) return {};
+		map<string, string> keyboard = {{"2", "abc"}, {"3", "def"}, {"4", "ghi"}, {"5", "jkl"}, {"6", "mno"}, {"7", "pqrs"}, {"8", "tuv"}, {"9", "wxyz"}};
+		vector<string> result = {""};
+		for(auto digit : digits)
+		{
+			vector<string> temp;
+			string letters = keyboard[string(1,digit)];
+			for (auto ch : letters)
+			{
+				for (auto combination : result)
+				{
+					temp.push_back(combination + ch);
+				}
+			}
+			result = temp;
+		}
 		return (result);
 	}
 };
+
+
 
 
 int main()
 {
 
 	Solution ex;
-	vector<string> result = ex.letterCombinations("123");
+	vector<string> result = ex.letterCombinations("23");
 
 
-	for (const string& s : result)
+	for (auto it = result.begin(); it != result.end(); it++)
 	{
-		cout << s << endl;
+		cout << *it << endl;
 	}
 
 }
