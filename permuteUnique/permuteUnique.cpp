85c1dc46af3c7ab09143b9b5f01d3a0a0acfefa9
Muhammet Kose <muhakose@2-A-2.42heilbronn.de>
2024-08-07 14:44:08 +0200

    permute unique

--- permuteUnique/permuteUnique.cpp
+++ permuteUnique/permuteUnique.cpp
@@ -7,12 +7,46 @@ class Solution
 public:
 	vector<vector<int>> permuteUnique(vector<int> &nums)
 	{
+		vector<vector<int>> res;
+		sort(nums.begin(), nums.end());
+
+
+		return (permute(nums));
+	}
+
+	vector<vector<int>> permute(vector<int> &nums)
+	{
+		vector<vector<int>> res;
+
+		int size = nums.size();
+
+		do
+		{
+			res.push_back(nums);
+		} while (std::next_permutation(nums.begin(), nums.end()));
+		return (res);
 	}
 };
+
 
 int main()
 {
 	Solution ss;
+	vector<vector<int>> nums =
+	{
+		{1, 1, 2},
+		{1, 2, 3},
+		{1},
+		{0, 1}
+	};
+
+	for (auto num : nums)
+	{
+		cout << "result for : ";
+		print_container(num);
+		print_nested_container(ss.permuteUnique(num));
+		cout << ("-----------------\n");
+	}
 
 	return (0);
 }
