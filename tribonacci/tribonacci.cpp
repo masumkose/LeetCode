728b24ad83fbbdf941a5769c0411a277d3898518
Muhammet Kose <muhakose@1-B-2.42heilbronn.de>
2024-10-01 14:10:07 +0200

    tribonacci

--- tribonacci/tribonacci.cpp
+++ tribonacci/tribonacci.cpp
@@ -2,11 +2,45 @@
 
 #include "../include/include.hpp"
 
+class Solution
+{
+public:
+	int tribonacci(int n)
+	{
+		switch (n)
+		{
+		case 0:
+			return (0);
+			break;
+		case 1:
+			return (1);
+			break;
+		case 2:
+			return (1);
+			break;
+		default:
+			break;
+		}
+		vector<int> res(n + 1, 0);
 
+		res[0] = 0;
+		res[1] = 1;
+		res[2] = 1;
+
+		for (int i = 3; i <= n; i++)
+		{
+			res[i] = res[i - 1] + res[i - 2] + res[i - 3];
+		}
+		return (res[n]);
+	}
+};
 
 int main()
 {
 	Solution ss;
+
+
+	cout << ss.tribonacci(6) << '\n';
 
 	return (0);
 }
