2e4bce668686aafeb436b2db923f0faae5164d26
Muhammet Kose <muhakose@1-C-15.42heilbronn.de>
2024-10-21 14:10:05 +0200

    finduslength

---
+++ findLUSlength/findLUSlength.cpp
@@ -0,0 +1,19 @@
+
+
+#include "../include/include.hpp"
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if (a == b)
+            return -1;
+        return (a.length() > b.length() ? a.length() : b.length());
+    }
+};
+
+int main()
+{
+	Solution ss;
+
+	return (0);
+}
