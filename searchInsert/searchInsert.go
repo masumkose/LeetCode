488b622f445d07c5646b7f55feec249349a27286
muhammet <masumkose54@hotmail.com>
2025-01-19 17:31:27 +0100

    searchInsert.go

---
+++ searchInsert/searchInsert.go
@@ -0,0 +1,9 @@
+// Package main is the entry point for the Go application
+package main
+
+import "fmt"
+
+func main() {
+ // TODO: Implement the solution for searchInsert
+ fmt.Println("Hello, Go!")
+}
