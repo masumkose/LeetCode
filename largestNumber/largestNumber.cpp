67df41aeed600529639b211525289f28ee61e5c5
Muhammet Kose <muhakose@1-A-26.42heilbronn.de>
2025-02-20 14:54:28 +0100

    largestNumber

---
+++ largestNumber/largestNumber.cpp
@@ -0,0 +1,51 @@
+
+#include "../include/include.cpp"
+
+bool compareByLeftDigitOrder(int a, int b) {
+    std::string sa = std::to_string(a);
+    std::string sb = std::to_string(b);
+
+    return sa + sb > sb + sa;
+}
+
+
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        string str;
+
+        sort(nums.begin(), nums.end(), compareByLeftDigitOrder);
+        for (auto num : nums) {
+            str = str + to_string(num);
+        }
+        if (str[0] == '0')
+            return "0";
+        return str;
+    }
+};
+
+
+
+int main() {
+
+
+    Solution ss = Solution();
+
+    vector<vector<int>> numss =
+    {
+        {10,2},
+        {3,30,34,5, 9, 98},
+        {0,0},
+        {0,0,0}
+    };
+
+
+    for (auto nums : numss)
+    {
+        cout << ss.largestNumber(nums) << '\n';
+    }
+
+
+    return 0;
+}
