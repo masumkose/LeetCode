600da4a6b6d8d44ff6e6d09d379810f695d8888d
Muhammet masum Köse <muhammetmasumkose@Muhammets-Mac-mini.local>
2025-04-06 21:05:52 +0200

    kthSmallest

---
+++ kthSmallest/kthSmallest.py
@@ -0,0 +1,44 @@
+
+from typing import List, Optional
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        i = 0
+        res = root.val
+
+        def dfs(node):
+            nonlocal i, res
+            if not node:
+                return
+            
+            dfs(node.left)
+            i += 1
+            if i == k:
+                res = node.val
+                return
+            dfs(node.right)
+        dfs(root)
+        return res
+
+
+root = TreeNode(3)
+root.left = TreeNode(1)
+root.right = TreeNode(4)
+root.left.right = TreeNode(2)
+
+ss = Solution()
+
+print(ss.kthSmallest(root, 1))
+
+
+
+
